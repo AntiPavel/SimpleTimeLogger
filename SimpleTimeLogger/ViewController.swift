41682672325dfde5054d0055eb1f0c204b461ded
antiPavel <pavel.antonov1980@icloud.com>
2018-07-15 11:40:37 +0300

    initial commit

---
+++ SimpleTimeLogger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleTimeLogger
+//
+//  Created by Antonov, Pavel on 7/15/18.
+//  Copyright © 2018 Pavel Antonov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
+
