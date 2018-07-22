5a58b423e98ff7e7dd03ce121309bd0701c8cf12
pavel <pavel.antonov1980@icloud.com>
2018-07-22 23:32:27 +0300

    mock data added

--- SimpleTimeLogger/Resources/ProjectConfiguration.swift
+++ SimpleTimeLogger/Resources/ProjectConfiguration.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import RealmSwift
 
 public let projects = ["Fly to Mars",
                 "Colonization of the Moon",
@@ -24,4 +25,25 @@ public let activities = ["Deep diving",
 public enum Status: String {
     case approved = "Approved"
     case notApproved = "Not Approved"
+}
+
+// fill some test logs for filtering
+
+struct TestLogsFiller {
+    var realm: Realm?
+    
+    func setTestLogs() {
+        guard let realm = realm else { return }
+        let storage: ReaderWriter = RealmStorageService(realm: realm)
+        for _ in 0...20 {
+            let timeLog = TimeLog()
+            timeLog.date = Calendar.current.date(byAdding: .year, value: -Int(arc4random_uniform(10)), to: Date())!
+            timeLog.project = projects[Int(arc4random_uniform(UInt32(projects.count)))]
+            timeLog.activity = activities[Int(arc4random_uniform(UInt32(activities.count)))]
+            timeLog.hours = String(arc4random_uniform(50))
+            storage.setLog(log: timeLog, isUpdate: false)
+        }
+    }
+    
+    
 }
